cb45981a7a21867310e6c0aa25d7ae7fb0b89ed6
toseth97 <oluwasheun9721@gmail.com>
2023-01-11 23:59:34 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""
-This file contains functions that
-reads n lines of a txt file and prints
-it to stdout
-"""
 
 
 def read_lines(filename="", nb_lines=0):
-    """
-    function that reads n lines
-    """
-    i = 0
-    with open(filename, encoding="utf-8") as myFile:
-        for line in myFile:
-            i += 1
-            print(line, end="")
-            if nb_lines == i:
-                break
+    with open(filename, "r", encoding="UTF-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+        for index in range(nb_lines):
+            print(f.readline(), end="")
