b6f449ef240a580de0f1f1d680ab0b6ebb4b9d85
toseth97 <oluwasheun9721@gmail.com>
2023-02-15 08:16:37 -0800

    0-list_databases.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- Script that lists all databases
--- using MYSQL server
+-- creating a database
+-- not allowed to use show and select should not fail if database exist
 CREATE DATABASE IF NOT EXISTS hbt_0c_0
