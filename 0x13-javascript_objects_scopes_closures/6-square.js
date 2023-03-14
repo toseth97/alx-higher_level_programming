da4f6d4f919d71c36bbf962835cbbd3004fc31e9
toseth97 <oluwasheun9721@gmail.com>
2023-03-14 12:43:51 -0700

    7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+const Square2 = require('./5-square');
+module.exports = class Square extends Square2 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+};
